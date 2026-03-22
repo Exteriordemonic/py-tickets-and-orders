beda1ac0e4a7d6f152993e66d28b791f3f952909
Kamil <kmirosz6@gmail.com>
2026-03-22 19:46:17 +0100

    Refactor type hints and improve transaction handling in service files
    - Updated return type hints for `get_cinema_halls`, `get_movies_sessions`, and `get_orders` functions to specify the model types.
    - Simplified the `create_movie` function by removing unnecessary transaction context manager.
    - Enhanced `create_order` function to use the user model directly and improved readability.

--- services/order.py
+++ services/order.py
@@ -1,30 +1,34 @@
 from datetime import datetime
+from django.contrib.auth import get_user_model
 from django.db.models import QuerySet
-from db.models import Order, Ticket, User, MovieSession
+from db.models import Order, Ticket, MovieSession
 from django.db import transaction
 
 
+user = get_user_model()
+
+
+@transaction.atomic
 def create_order(
     tickets: list[dict], username: str, date: str = datetime.now()
 ) -> Order:
-    with transaction.atomic():
-        user = User.objects.get(username=username)
+    db_user = user.objects.get(username=username)
 
-        order = Order.objects.create(user=user)
-        order.created_at = date
-        order.save(update_fields=["created_at"])
-        for ticket in tickets:
-            Ticket.objects.create(
-                movie_session=MovieSession.objects.get(
-                    id=ticket.get("movie_session")
-                ),
-                order=order,
-                row=ticket.get("row"),
-                seat=ticket.get("seat"),
-            )
+    order = Order.objects.create(user=db_user)
+    order.created_at = date
+    order.save(update_fields=["created_at"])
+    for ticket in tickets:
+        Ticket.objects.create(
+            movie_session=MovieSession.objects.get(
+                id=ticket.get("movie_session")
+            ),
+            order=order,
+            row=ticket.get("row"),
+            seat=ticket.get("seat"),
+        )
 
 
-def get_orders(username: str | None = None) -> QuerySet:
+def get_orders(username: str | None = None) -> QuerySet[Order]:
     if not username:
         return Order.objects.all()
 
